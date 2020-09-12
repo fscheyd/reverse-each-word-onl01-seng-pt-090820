73b3f51b3f45c3a18020ef2d9bba65ac2c5de43e
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-09-12 17:30:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,7 +11,9 @@ def reverse_each_word(string)
   array = string.split(" ")
   test_array = []
   array.collect do|string|
-  test_array << string.reverse
-  end
+    test_array << string.reverse
+    end
   test_array.join(" ")
 end
+
+reverse_each_word
